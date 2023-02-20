b5a0ffdad5c45fbb000284551d311323142e1718
bDiana25 <diana.bejan@cncn.ro>
2023-02-20 15:07:48 +0200

    new commit

---
+++ Primalitateanumerelor/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Primalitateanumerelor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button = new System.Windows.Forms.Button();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Button
+            // 
+            this.Button.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Button.Location = new System.Drawing.Point(393, 286);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(191, 81);
+            this.Button.TabIndex = 0;
+            this.Button.Text = "Submit";
+            this.Button.UseVisualStyleBackColor = false;
+            this.Button.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(437, 191);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(102, 20);
+            this.numberTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(972, 535);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.TextBox numberTextBox;
+    }
+}
+
---
+++ Primalitateanumerelor/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primalitateanumerelor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, EventArgs e) {
+        int result = 0;
+            bool isNumber = Int32.TryParse(numberTextBox.Text,out result);
+            if(isNumber) {
+                bool isPrime = true;
+                int i;
+                for (i = 2; i <= result / 2; i++)
+                {
+                    if (result % i == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+                if (isPrime)
+                {
+                    this.BackColor = Color.Green;
+                }
+                else
+                {
+                    this.BackColor = Color.Red;
+                }
+            }
+
+
+            else
+            {
+                this.BackColor = Color.Yellow;
+                MessageBox.Show("S-a introdus un text");
+            }
+            numberTextBox.Clear();
+        }
+        }
+    
+}
+        
+
+
+
+
